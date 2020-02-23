b4dab52e9d95332e98c7b5c48809c3c9bd87baae
Nemanja <nemanja.vasic@codecentric.de>
2020-02-23 18:04:11 +0100

    Add mongoose dependency and create User model

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { notFound, errorHandler } = require('./middlewares');
 
 const app = express();
 
@@ -7,27 +8,10 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (_, res) => {
-  res.json('THIS IS THE INITAL ROUTE');
+  res.json({ message: 'THIS IS THE INITAL ROUTE' });
 });
 
-// Middlware for creating 404 Error, setting response status to 404 and forwaring to
-// the Error handler
-app.use((req, res, next) => {
-  const error = new Error(`Not Found - ${req.originalUrl}`);
-  res.status(404);
-  next(error);
-});
-
-// Error handler
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '' : err.stack,
-  });
-});
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  nickName: { type: String, required: true },
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  age: { type: Number, min: [18, 'User has to be adult'], required: false },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
