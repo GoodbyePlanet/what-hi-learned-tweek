96298221a6c1ab78e424e03160f5a61f5a94d310
Nemanja <nemanja.vasic@codecentric.de>
2020-02-23 13:58:20 +0100

    Add error middlware and error handler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,32 @@ const express = require('express');
 
 const app = express();
 
+// TODO: Check HELMET => https://www.npmjs.com/package/helmet
+
 const PORT = process.env.PORT || 3001;
 
-app.listen(() => console.log(`Listening at http://localhost:${PORT}`));
+app.get('/', (_, res) => {
+  res.json('THIS IS THE INITAL ROUTE');
+});
+
+// Middlware for creating 404 Error, setting response status to 404 and forwaring to
+// the Error handler
+app.use((req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+});
+
+// Error handler
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '' : err.stack,
+  });
+});
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
