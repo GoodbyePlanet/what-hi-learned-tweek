57f99808d0f4ad5a1ad7776329e7dc1ba218400e
Nemanja <nemanja.vasic@codecentric.de>
2020-03-04 23:57:21 +0100

    Add bycript, isemail packages and add password field to User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,14 +1,35 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
+const isemail = require('isemail');
+const bcrypt = require('bcrypt');
+
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema({
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    validate: {
+      validator: email => isemail(email),
+      message: 'Email is not valid',
+    },
+    required: true,
+  },
+  password: { type: String, required: true },
   nickName: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   age: { type: Number, min: [18, 'User has to be adult'], required: false },
 });
+
+userSchema.statics.generateHash = function(password) {
+  const salt = bcrypt.genSaltSync(SALT_WORK_FACTOR);
+  return bcrypt.hashSync(password, salt);
+};
+
+userSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 const User = mongoose.model('User', userSchema);
 
